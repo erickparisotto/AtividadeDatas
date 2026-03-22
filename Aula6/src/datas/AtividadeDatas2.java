403cc8758c6967b464bd9433b0c5a75eff4a3a8d
erickparisotto <erickparisotto2006@gmail.com>
2026-03-22 13:51:38 -0300

    Versão final da atividade

--- Aula6/src/datas/AtividadeDatas2.java
+++ Aula6/src/datas/AtividadeDatas2.java
@@ -2,22 +2,16 @@ package datas;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Calendar;
 
 public class AtividadeDatas2 {
 	
 	public static void main(String[] args) {
 		contaTempo(04, 01, 2027);
-	}
+	}	
 
 	public static void contaTempo(int diaFinal, int mêsFinal, int anoFinal) {
-Calendar hoje = Calendar.getInstance();
 		
-		int diaAtual = hoje.get(Calendar.DAY_OF_MONTH);
-		int mesAtual = hoje.get(Calendar.MONTH);
-		int anoAtual = hoje.get(Calendar.YEAR);
-		
-		LocalDate primeiraData = LocalDate.of(anoAtual, mesAtual, diaAtual);
+		LocalDate primeiraData = LocalDate.now();
 		LocalDate segundaData = LocalDate.of(anoFinal, mêsFinal, diaFinal);
 		Period periodo = Period.between(primeiraData, segundaData);
 		
--- AulaDatas/src/pack/Principal.java
+++ AulaDatas/src/pack/Principal.java
@@ -1,7 +1,10 @@
 package pack;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,20 +12,33 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		
-		System.out.println("Digite o dia que você nasceu: ");
-		int dia = teclado.nextInt();
-		System.out.println("Digite o mês que você nasceu: ");
-		int mes = teclado.nextInt();
-		System.out.println("Digite o ano que você nasceu: ");
-		int ano = teclado.nextInt();
+		//defino o formato da data com sdf
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setLenient(false);
+		String nascimento = new String("");
 		
-		calcularIdade(dia, mes, ano);
+		System.out.println("Digite sua data de nascimento: (dd/mm/aaaa)");
+		nascimento = teclado.nextLine();
+		
+		try {
+			//tento pegar a string e transformar em Date
+			Date nascimentoDate = sdf.parse(nascimento);
+			
+			//transformo o Date para LocalDate
+			LocalDate nascimentoLocalDate = nascimentoDate.toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate();
+			
+			calcularIdade(nascimentoLocalDate);
+			
+		} catch(Exception e) {
+			System.out.println("Data inválida, tente novamente!");
+		}
 		
 		teclado.close();
 	}
 	
-	public static void calcularIdade(int dia, int mes, int ano) {
-		LocalDate dataNascimento = LocalDate.of(ano, mes, dia);
+	public static void calcularIdade(LocalDate dataNascimento) {
 		LocalDate dataAtual = LocalDate.now();
 		Period periodo = Period.between(dataNascimento, dataAtual);
 		
@@ -33,5 +49,10 @@ public class Principal {
 		System.out.println("Você tem " + anosTotal + " ano(s), " 
 				+ mesesTotal + " mes(es) e "		
 				+ diasTotal + " dia(s) de vida.");
+		
+		//parte que mostra se for seu aniversário
+		if((diasTotal == 0) && (mesesTotal == 0)) {
+			System.out.println("Hoje é seu aniversário, parábens!");
+		}
 	}
 }
